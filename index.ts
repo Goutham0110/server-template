08e825800381c700c049e6a64aa7f540d452a7a2
Goutham <gouthamsaravananr@gmail.com>
2024-05-31 18:53:26 +0530

    mongodb connection established

--- index.ts
+++ index.ts
@@ -1,8 +1,16 @@
 import express from 'express';
-import configs from './src/config/config';
+import cors from 'cors';
+import bodyParser from 'body-parser';
+
+import initDatabase from './src/utils/connect-mongodb';
+import configs from './src/config/configs';
 import router from './src/routers';
 
 const app = express();
+initDatabase();
+
+app.use(cors());
+app.use(bodyParser.json());
 
 app.use('/api', router);
 
--- src/config/config.ts
+++
@@ -1,7 +0,0 @@
-require('dotenv').config();
-
-const configs = {
-    PORT: process.env.PORT ||  4000
-}
-
-export default configs;
